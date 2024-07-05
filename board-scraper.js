c7f8e2ebafaaaf67ad0ada22b601a36142200203
AvionShea <avionsheadesigns@gmail.com>
2024-07-05 00:56:46 -0400

    added code to clear default location and the ability to search by location.

--- board-scraper.js
+++ board-scraper.js
@@ -17,12 +17,15 @@ const { executablePath } = require('puppeteer');
     const page = await browser.newPage(); // opens new blank page
 
     const jobTitle = "Software Engineer"; //enter job tile searching for
+    const jobLocation = "Morrisville, NC" //enter city, state, zip code, or "remote" of location wanted
     const jobBoards = [
         {
             name: "Indeed", //website name
             url: "https://www.indeed.com/", //website URL
             searchBar: "input[id='text-input-what']", //search bar url
             searchBtn: "button[class='yosegi-InlineWhatWhere-primaryButton']", // search button url
+            searchLocation: "input[id='text-input-where']", //search by location
+            clearSearch: "#jobsearch > div > div.css-13s6tc1.eu4oa1w0 > div.css-1jk1vg0.eu4oa1w0 > div > div > span > span.css-16oh2fs.e6fjgti0", //clears default location
         }
     ];
 
@@ -31,11 +34,22 @@ const { executablePath } = require('puppeteer');
 
     for (const board of jobBoards) {
         const searchUrl = `${board.url}`
+        const searchBar = board.searchBar
+        const searchBtn = board.searchBtn
+        const searchLocation = board.searchLocation
+        const clearSearch = board.clearSearch
+
         await page.goto(searchUrl, { waitUntil: 'networkidle2' })
 
         await page.locator(searchBar).fill(jobTitle);
 
+        await page.locator(clearSearch).click();
+
+        await page.locator(searchLocation).fill(jobLocation);
+
         await page.locator(searchBtn).click();
+
+
     }
 
 })();
